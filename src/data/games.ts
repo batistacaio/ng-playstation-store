7f7a1d1eff5cf2723291fe3a2fc9528338f20cd3
Caio Batista <caio_b_s@hotmail.com>
2023-08-24 15:35:51 -0300

    fix: home responsiveness and card data binding

---
+++ src/data/games.ts
@@ -0,0 +1,50 @@
+export const games = [
+    {
+        id: 1,
+        cover: '../../../assets/img/bt-1.jpg',
+        label: 'Exclusive',
+        type: 'PS4 Digital',
+        price: 'R$ 159,99',
+        purchased: true
+    },
+    {
+        id: 2,
+        cover: '../../../assets/img/bt-4.jpg',
+        label: 'Digital',
+        type: 'PS3 | PS4',
+        price: 'R$ 189,99',
+        purchased: false
+    },
+    {
+        id: 3,
+        cover: '../../../assets/img/bt-5.jpg',
+        label: 'Digital',
+        type: 'PS3 | PS4',
+        price: 'R$ 199,99',
+        purchased: false
+    },
+    {
+        id: 4,
+        cover: '../../../assets/img/bt-bad-company-2.jpg',
+        label: 'Exclusive',
+        type: 'PS3 | PS4',
+        price: 'R$ 189,99',
+        purchased: false
+    },
+    {
+        id: 5,
+        cover: '../../../assets/img/bt-hardline.jpg',
+        label: 'Exclusive',
+        type: 'PS3',
+        price: 'R$ 169,99',
+        purchased: false
+    },
+    {
+        id: 6,
+        cover: '../../../assets/img/bg-3.jpg',
+        label: 'New',
+        type: 'PS5',
+        price: 'R$ 299,99',
+        purchased: false
+    }
+]
